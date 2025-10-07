260080fcf2a6cced8808fec2bf004985783b5adb
DanielBR0612 <danielfranciscobragadearaujo@gmail.com>
2025-10-07 17:13:18 -0300

    :sparkles: feat: exercicio de fixação:

--- biblioteca/leitura/models.py
+++ biblioteca/leitura/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=100)
+    ano = models.DateField()
+    genero = models.CharField(max_length=25)
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_nascimento = models.DateField()
+    nacionalidade = models.CharField(max_length=20)
+    
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    dt_cadastro = models.DateField(auto_now_add=True)
+
+class Emprestimo(models.Model):
+    livro = models.ForeignKey(Livro, related_name='Emprestimos')
+    usuario = models.ForeignKey(Usuario, related_name='Emprestimo')
+    dt_emprestimo = models.DateField(auto_now_add=True)
+    
+class Reserva(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE, related_name='Reservas')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='Reservas')
+    dt_reserva = models.DateField()
+
+class Multa(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='Multas')
+    valor = models.FloatField(default=0)
+    dt_pagamento = models.DateField()
+
+class Categoria(models.Model):
+    nome_categoria = models.CharField(max_length=15)
+    descricao = models.TextField()
+
+class Editora(models.Model):
+    nome_editora = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=100)
--- biblioteca/leitura/views.py
+++ biblioteca/leitura/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import *
+from serializers import *
 
-# Create your views here.
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class EditoraViewSet(viewsets.ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EmprestimoViewSet(viewsets.ModelViewSet):
+    queryset = Emprestimo.objects.all()
+    serializer_class = EmprestimoSerializer
+
+class ReservaViewSet(viewsets.ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = ReservaSerializer
+
+class MultaViewSet(viewsets.ModelViewSet):
+    queryset = Multa.objects.all()
+    serializer_class = MultaSerializer
