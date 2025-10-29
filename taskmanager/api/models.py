3b1eb4613ca73a48b3525cc29006d97952fa81dc
Daniel Braga <101554437+DanielBR0612@users.noreply.github.com>
2025-10-29 12:36:00 +0000

    :sparkles: feat: models

--- taskmanager/api/models.py
+++ taskmanager/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Projeto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao_projeto = models.CharField(max_length=100)
+
+class Usuario(AbstractUser):
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, related_name="membros")
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=30)
+    descricao_tarefa = models.CharField(max_length=15)
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, related_name="tarefas")
