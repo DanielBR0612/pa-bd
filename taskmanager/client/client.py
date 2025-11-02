ad90c69d7c9806b05ac542c7b91c15577b323294
DanielBR0612 <danielfranciscobragadearaujo@gmail.com>
2025-11-01 22:39:41 -0300

    :sparkles: feat: CR feitos

--- taskmanager/client/client.py
+++ taskmanager/client/client.py
@@ -3,7 +3,7 @@ import json
 
 def menu():
     print("Escolha uma opção:")
-    print("1 - signup   2 - login.  3 - testar o token:")
+    print("1 - criar projeto  2 - listar projetos   3 - criar tarefa   4 - listar tarefas   5 - testar token:")
     
     return int(input())
 
@@ -11,18 +11,34 @@ URL = ''
 URL_signup = "http://127.0.0.1:8000/api/signup/" 
 URL_login = "http://127.0.0.1:8000/api/login/"
 URL_test = "http://127.0.0.1:8000/api/test_token/"
+URL_projetos = "http://127.0.0.1:8000/api/projetos/"
+URL_tarefas = "http://127.0.0.1:8000/api/tarefas/"
 
 def main():
+    abrirconta = input("Gostaria de criar uma conta? (sim/não) ")
+    if abrirconta.lower() == "sim":
+        URL = URL_signup
+        signup(URL)
+    elif abrirconta.lower() == "nao":
+        URL = URL_login
+        login(URL)
+        
     op = 0
-    while op != 3:
+    while op != 9:
         op = menu()
         if op == 1:
-            URL = URL_signup
-            signup(URL)
+            URL = URL_projetos
+            criar_projeto(URL)
         if op == 2:
-            URL = URL_login
-            login(URL)
+            URL = URL_projetos
+            requisicao_get(URL)
         if op == 3:
+            URL = URL_tarefas
+            criar_tarefa(URL)
+        if op == 4:
+            URL = URL_tarefas
+            requisicao_get(URL)
+        if op == 5:
             URL = URL_test
             if not token_salvo:
                 print("\nERRO: Você precisa fazer login (Opção 2) primeiro.")
@@ -41,7 +57,7 @@ def signup(URL):
         "email": email
     }
     
-    requisicao(URL, novo_usuario)
+    requisicao_post(URL, novo_usuario)
 
 def login(URL):
     username = str(input("Digite o seu username: "))
@@ -52,7 +68,31 @@ def login(URL):
         "password": senha,
     }
     
-    requisicao(URL, usuario)
+    requisicao_post(URL, usuario)
+
+def criar_projeto(URL):
+    nome = str(input("Digite o nome do projeto: "))
+    descricao = str(input("Digite a descrição do projeto: "))
+    
+    projeto = {
+        "nome": nome,
+        "descricao_projeto": descricao
+    }
+    
+    requisicao_post(URL, projeto)
+
+def criar_tarefa(URL):
+    titulo = str(input("Digite o titulo da tarefa: "))
+    descricao = str(input("Digite a descricao da tarefa: "))
+    id_projeto = int(input("Digite o id do projeto: "))
+    
+    tarefa = {
+        "titulo": titulo,
+        "descricao_tarefa": descricao,
+        "projeto": id_projeto
+    }
+    
+    requisicao_post(URL, tarefa)
 
 def testar_token(URL, token):
     print(f"\nEnviando requisição GET para {URL} com token {token}")
@@ -77,7 +117,23 @@ def testar_token(URL, token):
         print(f"\nERRO: Não foi possível conectar a {URL}")
 
     
-def requisicao(URL, user):
+def requisicao_get(URL):
+    try:
+        response = requests.get(URL)
+        
+        print(f"\nStatus Code: {response.status_code}")
+        
+        if response.status_code == 200:
+            print("RESPOSTA (sucesso):")
+            print(response.text) 
+        else:
+            print("RESPOSTA (falha):")
+            print(response.text) 
+
+    except requests.exceptions.ConnectionError:
+        print(f"\nERRO: Não foi possível conectar a {URL}")
+
+def requisicao_post(URL, user):
     global token_salvo
     try:
         response = requests.post(URL, json=user)
